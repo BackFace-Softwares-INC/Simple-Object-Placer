d85837fb86991a1fe61bbdae47ed305ed1c44033
Mun <67842288+MunFaill@users.noreply.github.com>
2025-08-03 20:38:47 -0300

    Alpha 0.0.2 update | New UI
    Added a new UI using file tree

--- addons/BFObjectPlacer/ui/scirpts/ui.gd
+++ addons/BFObjectPlacer/ui/scirpts/ui.gd
@@ -1,20 +1,65 @@
 @tool
-extends VBoxContainer
+extends HBoxContainer
 
 @onready var enabled : bool = false
-@onready var ResourcePicker : EditorResourcePicker = %BFOPResourcePicker
 @onready var OffsetX : SpinBox = %X
 @onready var OffsetY : SpinBox = %Y
 @onready var OffsetZ : SpinBox = %Z
+@onready var tree : Tree = %Tree
+
+var object_path
 
 var GIZMO : MeshInstance3D = null
 var gizmo_scene := preload("res://addons/BFObjectPlacer/gizmo/gizmo.tscn")
 
+func _ready() -> void:
+	tree.set_hide_root(true)
+	tree.set_columns(1)
+	tree.set_column_title(0,"Object Placer")
+	tree.connect("item_selected", _on_item_selected)
+
+func add_items_to_tree(path: String, parent: TreeItem) -> void:
+	var dir := DirAccess.open(path)
+	if dir == null:
+		print("Can't open: ", path)
+		return
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+	while file_name != "":
+		if file_name.begins_with("."):
+			file_name = dir.get_next()
+			continue
+
+		var full_path := path.path_join(file_name)
+		var is_folder := dir.current_is_dir()
+
+		var item := tree.create_item(parent)
+		item.set_text(0, file_name)
+		item.set_metadata(0, full_path)
+
+		var icon: Texture2D
+		if is_folder:
+			icon = EditorInterface.get_editor_theme().get_icon("Folder", "EditorIcons")
+		else:
+			icon = EditorInterface.get_editor_theme().get_icon("File", "EditorIcons")
+
+		item.set_icon(0, icon)
+
+		if is_folder:
+			add_items_to_tree(full_path, item)
+
+		file_name = dir.get_next()
+	dir.list_dir_end()
 
 func _on_enable_button_toggled(toggled_on: bool) -> void:
 	enabled = toggled_on
 	var edited_scene = EditorInterface.get_edited_scene_root()
 	if enabled:
+		tree.clear()
+
+		var root := tree.create_item()
+		add_items_to_tree("res://", root)
 		if not GIZMO:
 			GIZMO = gizmo_scene.instantiate()
 		edited_scene.add_child(GIZMO)
@@ -42,10 +87,10 @@ func collision() -> Dictionary:
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and not event.is_echo() and enabled and ResourcePicker.edited_resource != null:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and not event.is_echo() and enabled and object_path != null:
 			
 			if collision():
-				var obj = ResourcePicker.edited_resource.instantiate()
+				var obj = load(object_path).instantiate()
 				obj.position = collision().get("position") + Vector3(OffsetX.value, OffsetY.value, OffsetZ.value)
 					
 				var edited_scene = EditorInterface.get_edited_scene_root()
@@ -58,3 +103,10 @@ func _input(event):
 				undo_redo.add_do_method(obj, "set_owner", edited_scene)
 				undo_redo.add_undo_method(edited_scene, "remove_child", obj)
 				undo_redo.commit_action()
+
+func _on_item_selected() -> void:
+	var selected = tree.get_selected()
+	if selected:
+		print("A")
+		var path = selected.get_metadata(0)
+		object_path = path
